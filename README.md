9a0b80b841d8a2824a77726458a17e086f111add
Ricardo Band <email@ricardo.band>
2016-05-27 02:46:09 +0200

    switched from select2 to selectize. js files updated. requirements updated. homing implemented in frontend.

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # cug control
 
 ![cyber cug control](cybersteuerung_v0.1.0.png)
+
+
+## Website (js) -> Backend (Flask)
+
+  - connect()
+  - disconnect()
+  - home(num)
+  - homeall()
+  - update(json)
+
+## Website (js) <- Backend (Flask)
+
+  - update(json)
+  - reset()
+
+
+## Backend (Pyserial) -> Arduino
+
+  - led(r, g, b, ww)
+  - move(leave, pos)
+  - home(leave)
+
+## Backend (Pyserial) <- Arduino
+
+  - \#led(r, g, b, ww)
+  - \#move(leave, pos)
+  - \#home(leave)
--- app.py
+++ app.py
@@ -12,11 +12,16 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = RotatingFileHandler('cug.log', maxBytes=1048576, backupCount=2)
-handler.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('cug.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s - %(levelname)-5s - %(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-logger.addHandler(handler)
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 
 @app.route('/')
@@ -24,27 +29,43 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('broadcast', namespace='/cyber')
-def broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('response', {'data': bytes(message['data'], 'utf-8'), 'count': session['receive_count']}, broadcast=True)
+@socketio.on('connected')
+def handle_my_custom_event():
+    logger.info('client connected')
+
+@socketio.on('update')
+def handle_update_event(json):
+    logger.info('got an update. sending it to the others.')
+    logger.debug('content was: ' + str(json))
+    emit('update', json, broadcast=True)
+
+@socketio.on('home')
+def handle_update_event(target):
+    logger.info('homing event received')
+    logger.debug('content was: ' + target)
 
 
-@socketio.on('disconnect', namespace='/cyber')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('response', {'data': bytes('disconnected', 'utf-8'), 'count': session['receive_count']})
-    disconnect()
+#@socketio.on('broadcast', namespace='/cyber')
+#def broadcast_message(message):
+#    session['receive_count'] = session.get('receive_count', 0) + 1
+#    emit('response', {'data': bytes(message['data'], 'utf-8'), 'count': session['receive_count']}, broadcast=True)
 
 
-@socketio.on('connect', namespace='/cyber')
-def test_connect():
-    emit('response', {'data': bytes('connected', 'utf-8'), 'count': 0})
+#@socketio.on('disconnect', namespace='/cyber')
+#def disconnect_request():
+#    session['receive_count'] = session.get('receive_count', 0) + 1
+#    emit('response', {'data': bytes('disconnected', 'utf-8'), 'count': session['receive_count']})
+#    disconnect()
 
 
-@socketio.on('disconnect', namespace='/cyber')
-def test_disconnect():
-    print('disconnected')
+#@socketio.on('connect', namespace='/cyber')
+#def test_connect():
+#    emit('response', {'data': bytes('connected', 'utf-8'), 'count': 0})
+
+
+#@socketio.on('disconnect', namespace='/cyber')
+#def test_disconnect():
+#    print('disconnected')
 
 
 if __name__ == '__main__':
