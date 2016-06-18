328f040ea3f240500e66bd62ffefc3fa45005066
Uwe Kamper <me@uwekamper.de>
2016-06-18 08:48:40 +0000

    early morning commit

--- roloboxd/roloboxd.py
+++ roloboxd/roloboxd.py
@@ -7,8 +7,14 @@ import sys
 import asyncio
 import serial.aio
 import RPi.GPIO as GPIO
+import configparser
+import json
+
+DRUM_CONFIGS_DIR = os.path.join(os.path.dirname(__file__), 'drum_configs')
+drums = []
 
 def setup():
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(23, GPIO.OUT)#, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(24, GPIO.OUT)#, pull_up_down=GPIO.PUD_DOWN)
@@ -23,6 +29,104 @@ def shutdown():
     GPIO.output(23, GPIO.LOW)
     GPIO.output(24, GPIO.LOW)
 
+
+def get_drum_by_address(addr):
+    global drums
+    for drum in drums:
+        if drum.address == addr:
+            return drum
+    return None
+
+class Drum(object):
+    def __init__(self, name, address, pages, sensor_shift, strings):
+        self.name = name
+        self.address = int(address)
+        self.pages = int(pages)
+        self.strings = strings
+        self.sensor_shift = sensor_shift
+        if len(strings) < pages:
+            self.strings = strings + ["n/a"] * (pages - len(strings))
+        self.current_page = 0
+
+    def get_current_page(self):
+        """
+        Get a tuple of current page number and page string
+        """
+        return self.current_page, strings[current_page]
+
+    def set_current_page(self, page):
+        """
+        Change the current page of this drum
+        """
+        self.current_page = page
+
+    def get_available_pages(self):
+        """
+        Get a list of all available pages with their page numbers.
+        Filter out the empty pages
+        """
+        ps = filter(lambda x: x != '', enumerate(self.strings))
+        retval = []
+        for p in ps:
+            retval.append({"index": p[0], "label": p[1]})
+        return retval
+
+    def get_status(self):
+        """
+        Get a dict of the current state for JSON serilization.
+        """
+        return {
+            'name': self.name,
+            'address': self.address,
+            'pages': self.pages,
+            'current_page': self.current_page,
+            'current_label': self.strings[self.current_page]
+        }
+
+    def advance_pages(self, num):
+        self.current_page = (self.current_page + num) % self.pages
+
+    def get_index_by_label(self, label):
+        for index, bla in enumerate(self.strings):
+            if bla == label:
+                return index
+
+        return None 
+
+    
+    def __repr__(self):
+        return '<Drum name={} addr={} pages={}>'.format(self.name, self.address, self.pages)
+
+
+def read_strings(filename):
+    strings = []
+    infh = open(filename, mode='r')
+    for line in infh.readlines():
+       strings.append(line.strip())
+    return strings
+
+def iterate_drum_configs():
+    drums = []
+    for root, dirs, files in os.walk(DRUM_CONFIGS_DIR, topdown=False):
+        for name in files:
+            full_path = os.path.join(root, name)
+            basename, ext = os.path.splitext(name)
+            if ext == '.conf':
+                print('found {}'.format(name))
+                config = configparser.ConfigParser()
+                config.read(full_path)
+
+                strings_path = os.path.join(root, '{}.strings'.format(basename))
+                strings = read_strings(strings_path)
+
+                d = Drum(config.get('Drum', 'name'), config.get('Drum', 'address'), 
+                         config.getint('Drum', 'pages'), config.get('Drum', 'sensor_shift'), 
+                         strings)
+            
+                drums.append(d)
+    return sorted(drums, key=lambda x: x.address)
+
+
 class RoloboxProtocol(asyncio.Protocol):
 
     def connection_made(self, transport):
@@ -31,8 +135,51 @@ class RoloboxProtocol(asyncio.Protocol):
         self.transport = transport
 
     def data_received(self, data):
+        global drums
         print('{}: {!r}'.format(self.transport.get_extra_info('peername'), data.decode()))
-        asyncio.async(self.send_message(data))
+        msg = data.decode().strip()
+        if msg == 'status':
+            status = [d.get_status() for d in drums]
+            json_status = json.dumps(status) + '\n'
+            self.transport.write(json_status.encode('utf-8'))
+        elif msg.startswith('labels'):
+            cmd, address = msg.split(' ', 1)
+            drum = get_drum_by_address(int(address))
+            json_status = json.dumps(drum.get_available_pages()) + "\n"
+            self.transport.write(json_status.encode('utf-8'))
+        elif msg.startswith('go'):
+            cmd, address, index = msg.split(' ', 2)
+            drum = get_drum_by_address(int(address))
+            try:
+                index = int(index)
+            except ValueError:
+                lbl = index
+                index = drum.get_index_by_label(lbl)
+                print("found {} as {}".format(lbl, index))
+            drum.current_page = 0
+            if index == 0:
+                data = '{}/{}/0\n'.format(int(address), drum.sensor_shift)
+            else:
+                adv = index
+                drum.advance_pages(adv)
+                data = '{}/{}/0\n'.format(int(address), drum.sensor_shift)
+                data += '{}/{}/adv\n'.format(int(address), drum.sensor_shift, adv)
+            asyncio.async(self.send_message(data.encode('utf-8')))
+        else:
+            address, advance_pages = msg.split('/', 1)
+            address = int(address)
+            advance_pages = int(advance_pages)
+            print('advance by {} on {}'.format(advance_pages, address))
+            drum = get_drum_by_address(address)
+            if drum is None:
+                self.transport.write(b"error: wrong address\n")
+                return
+            if advance_pages > 100:
+                self.transport.write(b"error: too many pages\n")
+                return
+
+            drum.advance_pages(advance_pages)
+            asyncio.async(self.send_message(data))
 
     @asyncio.coroutine
     def send_message(self, message):
@@ -56,6 +203,7 @@ class SerialProtocol(asyncio.Protocol):
         asyncio.get_event_loop().stop()
 
 
+drums = iterate_drum_configs()
 setup()
 loop = asyncio.get_event_loop()
 socket_coroutine = loop.create_server(RoloboxProtocol, '0.0.0.0', 8888)
